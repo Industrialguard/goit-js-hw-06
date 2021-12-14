c2d11ea0f3aada259c0f46fa02fa6ca4d2cf06a4
Industrialguard <industrialguard@gmail.com>
2021-12-15 02:02:12 +0300

    Task 8 is done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const loginFormRef = document.querySelector(".login-form");
+const warningMessage = "Invalid Email or Password! Try again please!";
 
+const onLoginFormRefChange = function (change) {
+  change.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = change.currentTarget;
+
+  const dataFormResult = {};
+  const formData = new FormData(change.currentTarget);
+
+  if (email.value && password.value) {
+    formData.forEach((value, name) => {
+      dataFormResult[name] = value;
+    });
+
+    loginFormRef.reset();
+    return console.log(dataFormResult);
+  }
+
+  return alert(warningMessage);
+};
+
+loginFormRef.addEventListener("submit", onLoginFormRefChange);
