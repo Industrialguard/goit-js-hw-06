3d010b7f4e65c6c5919e938f23ef23c21d2b397c
Industrialguard <industrialguard@gmail.com>
2021-12-16 10:43:57 +0300

    Task 2 fix

--- js/task-02.js
+++ js/task-02.js
@@ -7,17 +7,6 @@ const ingredients = [
   'Condiments',
 ];
 
-/*const listEl = document.querySelector(`ul`)
-console.log(listEl);
-
-for (let i = 0; i < ingredients.length; i += 1) {
-  const itemListEl = document.createElement(`li`);
-
-  itemListEl.classList.add(`item`);
-  itemListEl.textContent = ingredients[i];
-  listEl.append(itemListEl);
-}*/
-
 const listEl = document.querySelector(`ul`);
 
 const items = ingredients.map(el => {
